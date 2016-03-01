115fd38828978dbbfaece49d98652ff9dd469ad6
Kieran Gallagher <kags1995@hotmail.co.uk>
2016-03-01 10:09:11 +0000

    Camera, Getters and setters

--- Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/Anthrocratos_UE4.h
+++ Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/Anthrocratos_UE4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_ANTHROCRATOS_UE4_H__
 #define __AUTO_ANTHROCRATOS_UE4_H__
-//Last Write: 26/02/2016 19:40:13 +00:00
-#include "S:\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
+//Last Write: 07/02/2016 15:33:17 +00:00
+#include "C:\Users\user\Game Development\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
 #endif//__AUTO_ANTHROCRATOS_UE4_H__
