ee4e208a74afa695c7cea912bb4faea913172692
Kieran Gallagher <kags1995@hotmail.co.uk>
2016-03-08 10:54:13 +0000

    Updated gamespawn and player stats

--- Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/Anthrocratos_UE4.h
+++ Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/Anthrocratos_UE4.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_ANTHROCRATOS_UE4_H__
 #define __AUTO_ANTHROCRATOS_UE4_H__
-//Last Write: 03/03/2016 10:13:19 +00:00
-#include "D:\GamesDevelopment\IP3 project\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
+//Last Write: 07/02/2016 15:33:17 +00:00
+#include "C:\Users\user\Game Development\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
 #endif//__AUTO_ANTHROCRATOS_UE4_H__
