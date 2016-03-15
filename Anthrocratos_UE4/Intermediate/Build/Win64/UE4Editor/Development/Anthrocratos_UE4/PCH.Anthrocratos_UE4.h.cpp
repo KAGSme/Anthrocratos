25ef1f2f5ab562621fb90c1edd810a9ceac11b62
Kieran Gallagher <kags1995@hotmail.co.uk>
2016-03-15 20:38:24 +0000

    quick fixes

--- Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/PCH.Anthrocratos_UE4.h.cpp
+++ Anthrocratos_UE4/Intermediate/Build/Win64/UE4Editor/Development/Anthrocratos_UE4/PCH.Anthrocratos_UE4.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\user\Game Development\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
+#include "D:\GamesDevelopment\IP3 project\Anthrocratos\Anthrocratos_UE4\Source\Anthrocratos_UE4\Anthrocratos_UE4.h"
